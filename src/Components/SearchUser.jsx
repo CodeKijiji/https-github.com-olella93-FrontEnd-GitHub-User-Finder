373b30078563d05d6448f4a62afd8a872dbd40d3
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-21 16:29:20 +0300

    username

---
+++ src/Components/SearchUser.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SearchUser = () => {
+    const [username, setUsername] = useState('');
+  return (
+    <div>SearchUser</div>
+  )
+}
+
+export default SearchUser
