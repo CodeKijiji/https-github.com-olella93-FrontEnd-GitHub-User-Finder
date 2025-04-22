a62aea40ef686172f086b2d7c21db27e56b3f3f4
Roy Richard <olella93@gmail.com>
2025-04-22 10:34:23 +0300

    Update RepoList.jsx

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react'
-
-const RandomImage = () => {
-    const [image, setImage] = React.useState("")
-
-    useEffect(()=>{
-        fetch("")
-        .then(res => res.json())
-        .then(data => setImage(data.message))
-        .catch(e => console.log(e))
-    }, [])
-
-    if(!image)return <h1>Loading...</h1>
+function RepoList({ repos }) {
   return (
-    <div>
-        <img src={image} alt="" />
+    <div className="repo-list">
+      <h3 className="repo-heading">Public Repositories</h3>
+      <div className="repo-grid">
+        {repos.map((repo) => (
+          <div key={repo.id} className="repo-card">
+            <a
+              href={repo.html_url}
+              target="_blank"
+              rel="noreferrer"
+              className="repo-link"
+            >
+              {repo.name}
+            </a>
+            <p className="repo-description">
+              {repo.description || "No description"}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default RepoList
+export default RepoList;
