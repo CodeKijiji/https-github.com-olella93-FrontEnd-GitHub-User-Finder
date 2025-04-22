aa71fd853c92fd1307ac293f5151ba1b2433d6d9
tjayearl <iamtjayearl@gmail.com>
2025-04-22 09:41:01 +0300

    Update usercard.jsx

--- src/components/Usercard.jsx
+++ src/components/Usercard.jsx
@@ -6,9 +6,11 @@ const UserCard = ({ user }) => {
 
   return (
     <div className="user-card">
-      <img src={user.avatar_url} alt={`${user.login}'s avatar`} className="avatar" />
-      <h2>{user.name || user.login}</h2>
+      <img src={user.avatar_url} alt={`${user.login}'s avatar`} className="avatar" width={"100"}/>
+      <h2>{user.name || "No name available."}</h2>
+      <p>@{user.login}</p>
       <p className="bio">{user.bio || 'No bio available.'}</p>
+      <p><strong>Public Repos:</strong> {user.public_repos}</p>
       <p><strong>Followers:</strong> {user.followers}</p>
       <p><strong>Following:</strong> {user.following}</p>
       <a href={user.html_url} target="_blank" rel="noopener noreferrer">
