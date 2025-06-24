b4fc1d65b196f9887816aaa1330504d7fd66dacd
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-24 15:07:49 +0300

    added loading spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,10 @@
 function Loader() {
-    return <p>Loading...</p>;
-  }
-  
-  export default Loader;
-  
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loader;
